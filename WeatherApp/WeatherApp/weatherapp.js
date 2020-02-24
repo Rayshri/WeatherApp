393406d2f2679ac5b012fc0736646101994990c0
Rayshri <36067658+Rayshri@users.noreply.github.com>
2020-02-24 13:35:29 +0100

    Add files via upload

--- WeatherApp/WeatherApp/weatherapp.js
+++ WeatherApp/WeatherApp/weatherapp.js
@@ -1,8 +1,8 @@
 function getLocation() {
-    if(navigator.geolocation) {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        console.log("failed");
+        alert("Get current location failed");
     }
     loadLocalStorageItems();
 }
@@ -10,17 +10,18 @@ function getLocation() {
 function showPosition(position) {
     const lat = position.coords.latitude;
     const long = position.coords.longitude;
-    const url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=0bda8162eb27abb639d50407de3249ba&units=metric';
+    const url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=5e8427b7948e766fcf7ec8ead638d7d1&units=metric';
     dataFetch(url, true);
 }
 
 function addNewCity() {
-    document.querySelector(".weatherSearch__button").addEventListener("click", function() {
-    const cityName = prompt("Please enter here your new city", " ");
-    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=0bda8162eb27abb639d50407de3249ba&units=metric";    
-    dataFetch(url);
-    // Give cityName with it so it can store
-    saveLocalStorageItem(cityName);
+    document.querySelector(".weather-search__button").addEventListener("click", function() {
+        const cityName = prompt("Please enter here your new city", " ");
+        const url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=5e8427b7948e766fcf7ec8ead638d7d1&units=metric";    
+        dataFetch(url);
+        // Give cityName with it so it can store
+        saveLocalStorageItem(cityName);
+        deleteLocalStorageItems(cityName);
     });
 }
 
@@ -40,7 +41,7 @@ function dataFetch(url, current = false) {
                     section.appendChild(cityname);
 
                     const temp = document.createElement("p");
-                    temp.innerHTML = data.main.temp + " °C";
+                    temp.innerHTML = Math.round(data.main.temp) + " °C";
                     section.appendChild(temp);
 
                     const iconurl = document.createElement("img");
@@ -51,13 +52,23 @@ function dataFetch(url, current = false) {
                     type.innerHTML = data.weather[0].description;
                     section.appendChild(type);
 
+                    const button = document.createElement("button");
+                    //button.classList.add("button");
+                    button.setAttribute("id", data.name);
+                    let newAtt = document.createAttribute("formtarget");
+                    newAtt.value = "_parent";
+                    button.setAttributeNode(newAtt);
+                    button.innerHTML += "Delete weather";
+                    section.appendChild(button);
+
                     if(current) {
+                        section.classList.add('currentweather');
                         section.innerHTML += " Current Weather";
                     }
                     return render(section);
                 });
             } else {
-                console.log("failed");
+                alert("Fetch failed");
             }
         });
 }
@@ -68,27 +79,27 @@ function render(element) {
 
 function getElement() {
     const section = document.createElement('section');
-    section.setAttribute('class', 'cityAdd');
+    section.setAttribute('class', 'cityadd');
     document.body.appendChild(section);
 }
 
 // Save the city name to the localStorage
 function saveLocalStorageItem(cityName) {
     // Get the current weateritems
-    let weatheritem = JSON.parse(window.localStorage.getItem("cityname"));
+    let weatheritem = JSON.parse(localStorage.getItem("cityname"));
     // If there is no weatheritems, create new Array
     if (!weatheritem) {
-      weatheritem = [];
+        weatheritem = [];
     };
     // Add the cityname to the array
     weatheritem.push(cityName);
     // Set the item
-    window.localStorage.setItem("cityname", JSON.stringify(weatheritem));
+    localStorage.setItem("cityname", JSON.stringify(weatheritem));
 }
 
 function loadLocalStorageItems() {
     // Get the weatheritems
-    const weatheritem = JSON.parse(window.localStorage.getItem("cityname"));
+    const weatheritem = JSON.parse(localStorage.getItem("cityname"));
     // If there are no weatheritems, do nothing
     if (!weatheritem) {
       return false;
@@ -96,11 +107,38 @@ function loadLocalStorageItems() {
     // Do something for each item
     weatheritem.forEach(function(cityName) {
       // Call the API to get the data
-      const url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=0bda8162eb27abb639d50407de3249ba&units=metric"; 
+      const url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=5e8427b7948e766fcf7ec8ead638d7d1&units=metric"; 
       // Call the fetch to generate Added weather based on the data
       dataFetch(url);
     });
 }
 
+function deleteLocalStorageItems(cityName) {
+    document.addEventListener("click", function(button) {
+        if(button.target && button.target.id === "button"){
+        // Get the current weateritems
+        let weatheritem = JSON.parse(localStorage.getItem("cityname"));
+        // If there is no weatheritems, create new Array
+        console.log(weatheritem);
+        if (!weatheritem) {
+            weatheritem = [];
+        };
+        const city = weatheritem.filter((item)=> {
+            if(cityName == item) {
+                return false;
+            } else {
+                return true;
+            }
+        });
+        // console.log(button.target);
+        //let filtered = weatheritem.filter("button");
+        // Set the item
+        localStorage.setItem("cityname", JSON.stringify(city));
+    } else {
+        //console.log("dit werkt niet");
+    }
+    });
+}
+// deleteLocalStorageItems(cityName);
 getLocation();
-addNewCity();
+addNewCity(); 
